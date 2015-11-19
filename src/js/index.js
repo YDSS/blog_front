4b650802ac10c12498258aad3c32264892c47e96
YDSS <y.dsheng10@gmail.com>
2015-11-19 22:30:57 +0800

    do nothing but try introduce redux-router

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,38 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { compose, createStore } from 'redux';
 import { Provider } from 'react-redux';
-// import { Router, Route, IndexRoute, Link } from 'react-router';
+import { Router, Route, IndexRoute, Link } from 'react-router';
+import { reduxReactRouter, routerStateReducer, ReduxRouter } from 'react-router';
 import App from './container/App.jsx';
-import configureStore from './store/configureStore';
-// import Content from './component/Content.jsx';
-// import Editor from './component/Editor.jsx';
-// import Article from './component/Article.jsx';
+// import configureStore from './store/configureStore';
+import rootReducer from './reducer/index';
 
-let store = configureStore();
+// let store = configureStore();
 let $wrap = document.getElementById('wrapper');
 
-render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    $wrap
+// <IndexRoute component={Content} />
+// <Route path='article/:id' component={Article} />
+
+const routes = (
+    <Route path='/' component={App}>
+        <Route path='home' component={Content} />
+        <Route path='edit' component={Editor} />
+    </Route>
 );
+
+const store = compose(
+    reduxReactRouter({
+        routers
+    })
+)(createStore)(rootReducer);
+
+// render(
+//     <Provider store={store}>
+//         <App />
+//     </Provider>,
+//     $wrap
+// );
 // render(
 //     <Router>
 //         <Route path='/' component={App}>
