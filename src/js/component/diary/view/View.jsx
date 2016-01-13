891e67dae7b4e870b7b4a9db3137e1fed41d799c
YDSS <y.dsheng10@gmail.com>
2016-01-13 23:05:28 +0800

    disable days for no diary to show

--- src/js/component/diary/view/View.jsx
+++ src/js/component/diary/view/View.jsx
@@ -3,12 +3,34 @@ import Calendar from 'rc-calendar';
 
 class View extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            noDiaryDate: [1, 5, 10]
+        };
+    }
+
+    onDateChange(date) {
+        console.log(date);
+    }
+
+    disabledDate(current) {
+        let {noDiaryDate} = this.state;
+
+        return noDiaryDate.find(day => day === current.fields[3]);
+    }
+
     render() {
         return (
             <div className="view">
                 <div className="content">Hi there</div>
                 <div className="calendar">
-                    <Calendar /> 
+                    <Calendar 
+                        showDateInput={false}
+                        onChange={this.onDateChange.bind(this)}
+                        disabledDate={this.disabledDate.bind(this)}
+                    /> 
                 </div>
             </div>
         );
