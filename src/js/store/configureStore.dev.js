af5741565b6a3afa7b10e956aa97226fcd00da8f
YDSS <y.dsheng10@gmail.com>
2016-02-13 10:21:39 +0800

    fix some bugs in diary part

---
+++ src/js/store/configureStore.dev.js
@@ -0,0 +1,21 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
+import {apiMiddleware} from 'redux-api-middleware';
+import { createHistory } from 'history';
+import { reduxReactRouter } from 'redux-router';
+import rootReducer from '../reducer/index';
+
+let logger = createLogger();
+// compose middlewares
+let middleware = [thunk, logger, apiMiddleware];
+// create store with initial state
+export default function configureStore(initialState) {
+    const store = compose(
+        applyMiddleware(...middleware),
+        // compose redux-rooter to enhance store
+        reduxReactRouter({ createHistory })
+    )(createStore)(rootReducer, initialState);
+        
+    return store;
+}
